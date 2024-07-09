9fcbf6d1a6c597d89ad0e2a03e2a7945d55c392e
Walter van der Broek <wbvanderbroek@st.noorderpoort.nl>
2024-07-09 14:19:29 +0200

    renaming

--- Assets/MultiplayerLobby/CallBackExample.cs
+++ Assets/MultiplayerLobby/CallBackExample.cs
@@ -3,7 +3,7 @@ using System.Collections.Generic;
 using Unity.Netcode;
 using UnityEngine;
 
-public class PlayerSpawnManager : MonoBehaviour
+public class CallBackExample : MonoBehaviour
 {
     private int amountOfPlayersSpawned;
     void Start()
